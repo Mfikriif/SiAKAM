c431ad79756505162ad8d98f37aa38dd15cb96c3
leanderarya <aryaajisadda@gmail.com>
2024-09-23 21:38:12 +0700

    Ganti Design Login!

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,16 +15,16 @@
         <div class="bg-white w-100 mx-auto my-auto rounded-xl shadow-lg p-8">
             <div class="text-center">
                 <img class="mx-auto" src="{{ asset('undipLogo.png') }}" alt="">
-                <h1 class="text-center text-3xl pt-5 tracking-wide">Sistem Informasi Akademik Mahasiswa (SiAkam)</h1>
+                <h1 class="text-center text-3xl pt-5 tracking-wide">Sistem Informasi Akademik Mahasiswa (SiAKAM)</h1>
                 <p class="text-center pt-2 text-slate-500 tracking-wider ">Universitas Diponegoro</p>
             </div>
 
-            <form method="POST" action="{{ route('login') }}">
+                <form method="POST" action="{{ route('login') }}">
                     @csrf
 
                     <div class="flex items-center border rounded-lg p-2 mt-5">
                         <img src="{{ asset('account.png') }}" alt="">
-                        <input type="text" name="email" placeholder="Username/Email" class="flex-1 outline-none bg-transparent pl-4" required autofocus>
+                        <input type="text" name="email" placeholder="Email" class="flex-1 border-none bg-transparent pl-4" required autofocus>
                     </div>
                     @error('email')
                         <span class="text-red-500 text-xs">{{ $message }}</span>
@@ -32,21 +32,17 @@
 
                     <div class="flex items-center border rounded-lg p-2 mt-3">
                         <img src="{{ asset('lock-outline.png') }}" alt="">
-                        <input type="password" name="password" placeholder="Password" class="flex-1 outline-none bg-transparent pl-4" required>
+                        <input type="password" name="password" placeholder="Password" class="flex-1 border-none bg-transparent pl-4" required>
                     </div>
                     @error('password')
                         <span class="text-red-500 text-xs">{{ $message }}</span>
                     @enderror
 
                     <button type="submit"
-                        class="border-solid border-2 border-slate-300 w-full h-12 mt-3 rounded-md text-slate-300 hover:border-sky-700 hover:text-sky-700">
-                        LOGIN
-                    </button>
+                    class="border-solid border-2 border-blue-700 w-full h-12 mt-3 rounded-md text-white bg-blue-600">
+                    LOGIN
+                </button>
                 </form>
-
-            <button class="w-full h-10 mt-4 rounded-md text-white bg-red-500 hover:bg-red-600 transition duration-200">Reset Password</button>
-            <p class="text-slate-400 font-light text-center mt-4">Belum memiliki akun?</p>
-            <p class="text-red-500 text-center">Daftar sekarang!</p>
         </div>
     </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Route::get('/dashboard', function () {
