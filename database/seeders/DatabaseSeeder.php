ad0c71868d988af54db01dd8eb145e951bc34043
leanderarya <aryaajisadda@gmail.com>
2024-10-21 11:21:26 +0700

    Mengisi Tabel Users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,82 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // You can create individual users like this:
+        User::create([
+            'name' => 'mahasiswa',
+            'email' => 'mahasiswa@gmail.com',
+            'password' => Hash::make('mahasiswa12345'), // Adjust accordingly, it's hashed
+            'mahasiswa' => 1,
+            'dekan' => 0,
+            'kaprodi' => 0,
+            'dosenwali' => 0,
+            'akademik' => 0,
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'dekan',
+            'email' => 'dekan@gmail.com',
+            'password' => Hash::make('dekan12345'),
+            'mahasiswa' => 0,
+            'dekan' => 1,
+            'kaprodi' => 0,
+            'dosenwali' => 0,
+            'akademik' => 0,
+        ]);
+
+        User::create([
+            'name' => 'kaprodi',
+            'email' => 'kaprodi@gmail.com',
+            'password' => Hash::make('kaprodi12345'),
+            'mahasiswa' => 0,
+            'dekan' => 0,
+            'kaprodi' => 1,
+            'dosenwali' => 0,
+            'akademik' => 0,
+        ]);
+
+        User::create([
+            'name' => 'akademik',
+            'email' => 'akademik@gmail.com',
+            'password' => Hash::make('akademik12345'),
+            'mahasiswa' => 0,
+            'dekan' => 0,
+            'kaprodi' => 0,
+            'dosenwali' => 0,
+            'akademik' => 1,
+        ]);
+
+        User::create([
+            'name' => 'dosenwali',
+            'email' => 'dosenwali@gmail.com',
+            'password' => Hash::make('dosenwali12345'),
+            'mahasiswa' => 0,
+            'dekan' => 0,
+            'kaprodi' => 0,
+            'dosenwali' => 1,
+            'akademik' => 0,
+        ]);
+
+        User::create([
+            'name' => 'dosenwali2',
+            'email' => 'dosenwali2@gmail.com',
+            'password' => Hash::make('dosenwali212345'),
+            'mahasiswa' => 0,
+            'dekan' => 0,
+            'kaprodi' => 1,
+            'dosenwali' => 1,
+            'akademik' => 0,
+        ]);
+
+        User::create([
+            'name' => 'dosenwali3',
+            'email' => 'dosenwali3@gmail.com',
+            'password' => Hash::make('dosenwali312345'),
+            'mahasiswa' => 0,
+            'dekan' => 1,
+            'kaprodi' => 0,
+            'dosenwali' => 1,
+            'akademik' => 0,
         ]);
     }
 }
