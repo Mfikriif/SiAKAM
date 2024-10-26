5260712f9b2a8236f6c0d92ca72f6c2233b65af9
Muhammad Fikri Firdaus <fikrifirdaus2112@gmail.com>
2024-10-26 12:50:23 +0700

    cdn

--- resources/views/mahasiswa/herReg.blade.php
+++ resources/views/mahasiswa/herReg.blade.php
@@ -10,6 +10,7 @@
     @vite('resources/js/app.js')
     <!-- Tambahkan SweetAlert2 CSS dan JS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Her-Registrasi</title>
 </head>
 
