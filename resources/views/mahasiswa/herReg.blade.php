734a9687bec8a7c3da8f27d3b62eb5a2f5d6ee52
Muhammad Fikri Firdaus <fikrifirdaus2112@gmail.com>
2024-10-26 12:47:11 +0700

    salah ngapus line bang wkwk

--- resources/views/mahasiswa/herReg.blade.php
+++ resources/views/mahasiswa/herReg.blade.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <!-- Tambahkan SweetAlert2 CSS dan JS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Her-Registrasi</title>
 </head>
 
